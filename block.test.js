b9e5274105d253996f3bb3c3a11e95de99a0b152
Caio B. Dutra <caio.dutra@facedigital.com.br>
2022-10-26 17:04:10 -0300

    Genesis Block made and TDD made on it.

--- block.test.js
+++ block.test.js
@@ -1,12 +1,12 @@
 const Block = require("./block");
-
-
+const { GENESIS_DATA } = require("./config");
 
 describe("Block", () => {
     const timestamp = "a-date";
     const lastHash = "foo-hash";
     const hash = "bar-hash";
     const data = ["blockchain", "data"];
+
     //@ Se a chave no valor é a mesma que o objeto, pode-se usar sintaxe curta.
     // const block = new Block({
     //     timestamp: timestamp,
@@ -14,12 +14,28 @@ describe("Block", () => {
     //     hash: hash,
     //     data: data,
     // });
-    const block = new Block({timestamp, lastHash, hash, data});
+
+    const block = new Block({ timestamp, lastHash, hash, data });
 
     it("has a timestamp, lastHash, hash, data, and a property", () => {
         expect(block.timestamp).toEqual(timestamp);
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+    });
+
+    describe("genesis()", () => {
+        //$ Funções estáticas são convenientes quando não precisa usar ou mudar dados de uma instância específica de uma classe, mas sim de uma classe em si.
+        const genesisBlock = Block.genesis();
+
+        console.log('genesisBlock', genesisBlock);
+
+        it("returns a Block instance", () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        });
+
+        it("returns the genesis data", () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        });
     });
 });
---
+++ config.js
@@ -0,0 +1,10 @@
+const Block = require("./block");
+
+const GENESIS_DATA = {
+    timestamp: 1,
+    lastHash: "-----",
+    hash: "hash-one",
+    data: [],
+};
+
+module.exports = { GENESIS_DATA };
