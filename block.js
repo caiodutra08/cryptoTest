573bfb204e4fb39583b260bc277d3ba6c74524b6
Caio B. Dutra <caio.dutra@facedigital.com.br>
2022-10-26 17:43:21 -0300

    Docs.

--- block.js
+++ block.js
@@ -1,6 +1,13 @@
 const { GENESIS_DATA } = require("./config");
 
 class Block {
+    /**
+     * [The constructor it intializes each block]
+     * @param  {[Date]} timestamp [The time when the block was created]
+     * @param  {[string]} lastHash [The hash from the previous block]
+     * @param  {[string]} hash [The hash from the current block]
+     * @param  {[array|object]} data [The data from the current block]
+     */
     constructor({ timestamp, lastHash, hash, data }) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
@@ -8,6 +15,10 @@ class Block {
         this.data = data;
     }
 
+    /**
+     * It returns a new instance of the block class with the genesis data.
+     * @returns A new instance of the Block class with the GENESIS_DATA as the data.
+     */
     static genesis() {
         return new this(GENESIS_DATA);
     }
@@ -16,7 +27,7 @@ class Block {
         return new this({
             timestamp: Date.now(),
             lastHash: lastBlock.hash,
-            data 
+            data,
         });
     }
 }
