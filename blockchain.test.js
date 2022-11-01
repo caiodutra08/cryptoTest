c398c2ffd567d9f95fb852cdc087dae8218ed83e
Caio B. Dutra <caio.dutra@facedigital.com.br>
2022-11-01 15:43:38 -0300

    Added a validation for the chain

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,11 @@ const Blockchain = require("./blockchain");
 const Block = require("./block");
 
 describe("Blockchain", () => {
-	const blockchain = new Blockchain();
+	let blockchain;
+
+	beforeEach(() => {
+		blockchain = new Blockchain();
+	});
 
 	it("contains a `chain` Array instance", () => {
 		expect(blockchain.chain instanceof Array).toBe(true);
@@ -16,9 +20,46 @@ describe("Blockchain", () => {
 		const newData = "foo bar";
 		blockchain.addBlock({ data: newData });
 
-		expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(
-			newData
-		);
+		expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
 	});
-    
+	describe("isValidChain()", () => {
+		describe("when the chain does not start with the genesis block", () => {
+			it("returns false", () => {
+				blockchain.chain[0] = { data: "fake-genesis" };
+
+				expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+			});
+		});
+
+		describe("when the chain starts with the genesis block and has multiple blocks", () => {
+			let dataInsert = ["Bears", "Beets", "Battlestar Galactica"];
+			beforeEach(() => {
+				dataInsert.forEach((info) => {
+					blockchain.addBlock({ data: info });
+				});
+			});
+
+			describe("and a lastHash reference has changed", () => {
+				it("return false", () => {
+					blockchain.chain[2].lastHash = "broken-lastHash";
+
+					expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+				});
+			});
+
+			describe("and the chain contains a block with an invalid field", () => {
+				it("retuns false", () => {
+					blockchain.chain[2].data = "some-bad-and-evil-data";
+
+					expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+				});
+			});
+
+			describe("and the chain does not contain any invalid blocks", () => {
+				it("returns true", () => {
+					expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+				});
+			});
+		});
+	});
 });
